2e3c61916f82761d6061829396ae7cdef288b910
AREL <AREL@B504-02>
2018-02-22 21:23:36 +0300

    DataGrid eklendi

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Customers> list;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            list = new List<Customers>()
+            {
+
+               new Customers() { AD="Semanur" , SOYAD="AŞÇI" },
+              new Customers()  { AD="Semanur" , SOYAD="AŞÇI" },
+              new Customers()  { AD="Semanur" , SOYAD="AŞÇI" },
+             new Customers()   { AD="Semanur" , SOYAD="AŞÇI" }
+        };
+
+            Customers lst = new Customers();
+            lst.AD = "aLİ";
+            lst.SOYAD = "KALABA";
+            list.Add(lst);
+        }
+       
+
+        private void btntikla_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Butona Tıklandı.");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(cmbBoxListe.SelectedItem.ToString());
+
+            MessageBox.Show(((ComboBoxItem)(cmbBoxListe.SelectedItem)).Content.ToString());
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            tb1.ItemsSource = list;
+        }
+    }
+
+    public class Customers
+    {
+        public string AD { get; set; }
+        public string SOYAD { get; set; }
+
+    }
+}
